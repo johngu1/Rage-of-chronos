0ea287416304bfbd64623486133fe0b7c6b352d6
johngu1 <johnnynguyenuwu@gmail.com>
2022-02-09 15:17:32 -0500

    watched project help video and completed the assignment template.
    now to work on the names of base "items"

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,14 +10,26 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+			//variable
 			bool exit = false;
+			int roomLocation = 0;
+
+			// arrays
+			string[] rooms = new string[5] { "room1", "room2" , "room3" , "room4" , "room5" };
+			string[] weapons = new string[4] { "weapon1", "weapon2" , "weapon3" , "weapon4" };
+			string[] potions = new string[2] { "potion1", "potion2"};
+			string[] treasures = new string[3] { "treasure1", "treasure2", "treasure3" };
+
+			//lists
+			List<string> items = new List<string>() { "item1", "item2", "item3", "item4" };
+			List<string> mobs = new List<string>() {  "mob1", "mob2", "mob3", "mob4", "mob5"};
 
 			//Main menu
 
 			while (exit == false)
 			{
 
-				Console.WriteLine("You are in ...");
+				Console.WriteLine($"You are in ...{rooms[roomLocation]}");
 				Console.WriteLine("1. Display Rooms");
 				Console.WriteLine("2. Display Weapon");
 				Console.WriteLine("3. Display Potion");
@@ -28,17 +40,121 @@ namespace ConsoleUI
 				Console.Write("Enter a choice > ");
 				string input = Console.ReadLine();
 
-				if (input == "1")
-				{
-					//TODO
-				}
-				else if (input == "2")
-				{
-					exit = true;
-				}
-				else
-				{
-					Console.WriteLine("Not a choice!");
+				switch(input.ToLower())
+                {
+					case "1":
+						foreach(string value in rooms)
+                        {
+							Console.WriteLine(value);
+						}
+						Console.WriteLine();
+						break;
+					case "2":
+						foreach (string value in weapons)
+						{
+							Console.WriteLine(value);
+						}
+						Console.WriteLine();
+						break;
+					case "3":
+						foreach (string value in potions)
+						{
+							Console.WriteLine(value);
+						}
+						Console.WriteLine();
+						break;
+					case "4":
+						foreach (string value in treasures)
+						{
+							Console.WriteLine(value);
+						}
+						Console.WriteLine();
+						break;
+					case "5":
+						foreach (string value in items)
+						{
+							Console.WriteLine(value);
+						}
+						Console.WriteLine();
+						break;
+					case "6":
+						foreach (string value in mobs)
+						{
+							Console.WriteLine(value);
+						}
+						Console.WriteLine();
+						break;
+					case "rooms":
+						foreach (string value in rooms)
+						{
+							Console.WriteLine(value);
+						}
+						Console.WriteLine();
+						break;
+					case "weapons":
+						foreach (string value in weapons)
+						{
+							Console.WriteLine(value);
+						}
+						Console.WriteLine();
+						break;
+					case "potions":
+						foreach (string value in potions)
+						{
+							Console.WriteLine(value);
+						}
+						Console.WriteLine();
+						break;
+					case "treasures":
+						foreach (string value in treasures)
+						{
+							Console.WriteLine(value);
+						}
+						Console.WriteLine();
+						break;
+					case "items":
+						foreach (string value in items)
+						{
+							Console.WriteLine(value);
+						}
+						Console.WriteLine();
+						break;
+					case "mobs":
+						foreach (string value in mobs)
+						{
+							Console.WriteLine(value);
+						}
+						Console.WriteLine();
+						break;
+					case "n":
+						if(roomLocation != 4)
+                        {
+							roomLocation++;
+                        }
+						else
+                        {
+							Console.WriteLine("You can not go further north");
+						}
+						Console.WriteLine();
+						break;
+					case "s":
+						if (roomLocation != 0)
+						{
+							roomLocation--;
+						}
+						else
+						{
+							Console.WriteLine("You can not go further south");
+						}
+						Console.WriteLine();
+						break;
+					case "7":
+						exit = true;
+						break;
+					default:
+						Console.WriteLine("Not a choice");
+						Console.WriteLine();
+						break;
 				}
 			}
 		}

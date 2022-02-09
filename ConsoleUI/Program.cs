58a8a5eef5ade696da949e7a7ee98c542e774a33
johngu1 <johnnynguyenuwu@gmail.com>
2022-02-09 14:22:37 -0500

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			bool exit = false;
+
+			//Main menu
+
+			while (exit == false)
+			{
+
+				Console.WriteLine("You are in ...");
+				Console.WriteLine("1. Display Rooms");
+				Console.WriteLine("2. Display Weapon");
+				Console.WriteLine("3. Display Potion");
+				Console.WriteLine("4. Display Treasure");
+				Console.WriteLine("5. Display Items");
+				Console.WriteLine("6. Display Mobs");
+				Console.WriteLine("7. Exit");
+				Console.Write("Enter a choice > ");
+				string input = Console.ReadLine();
+
+				if (input == "1")
+				{
+					//TODO
+				}
+				else if (input == "2")
+				{
+					exit = true;
+				}
+				else
+				{
+					Console.WriteLine("Not a choice!");
+				}
+			}
+		}
+    }
+}
